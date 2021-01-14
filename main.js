4f2420fccf0337b29eea4adc38dbc9f3064bd2e0
Prana Tantri <67408817+ProgrammingWithPranav@users.noreply.github.com>
2021-01-14 15:43:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,15 @@ var SpeechRecognition = window.webkitSpeechRecognition;
 
 var recognition = new SpeechRecognition();
 
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: 'png',
+    png_quality: 90
+});
+
+var camera = document.getElementById("camera");
+
 function start() {
     document.getElementById("voice-input").innerHTML = "";
     recognition.start();
@@ -13,4 +22,18 @@ recognition.onresult = function(event) {
     var Content = event.results[0][0].transcript;
     
     document.getElementById("voice-input").innerHTML = Content;
+
+    if(Content == "take my selfie") {
+        speak();
+        Webcam.attach(camera);
+    }
+}
+
+function speak() {
+    var Synth = window.speechSynthesis;
+
+    var speakData = "Taking your selfie in 5 seconds";
+
+    var speakThis = new SpeechSynthesisUtterance(speakData);
+    Synth.speak(speakThis);
 }
